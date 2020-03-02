8a3b1fbc2b1ed8758aa740c88f09fcd3729b6224
SALajes <sofialajes98@gmail.com>
2020-03-02 12:26:38 +0000

    Started LAB3 : Server

---
+++ LAB3/RemoteInterface.java
@@ -0,0 +1,9 @@
+package Server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteInterface extends Remote{
+    public String lookup(String DNS) throws RemoteException;
+    public int register(String DNS, String IP) throws RemoteException;
+}
---
+++ LAB3/Server.java
@@ -0,0 +1,63 @@
+package Server;
+
+import java.util.HashMap;
+import java.io.IOException;
+import java.net.*;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.RemoteObject;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server implements RemoteInterface{
+    private static HashMap<String,String> DNStable = new HashMap<String, String>();
+
+    private static RemoteObject remoteObject;
+
+    public static void main(String[] args) throws RemoteException{
+        if(args.length != 1){
+            System.out.println("Usage: java Server <remote_object_name>");
+            return;
+        }
+
+        try {
+            RemoteInterface stub = (RemoteInterface) UnicastRemoteObject.exportObject(this, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(args[0], stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public String lookup(String DNS) throws RemoteException{
+        if(check_table(request))
+            reply = request.IP_address;
+        else reply = "NOT_FOUND";
+
+        return reply;
+    }
+
+    public int register(String DNS, String IP) throws RemoteException{
+        if(!check_table(request)){
+            DNStable.put(DNS, IP);
+            reply = DNStable.size();
+        }
+        else reply = -1;
+
+        return reply;
+    }
+
+    private static boolean check_table(RequestPacket request){
+        if(DNStable.containsKey(request.DNS)){
+            request.IP_address = DNStable.get(request.DNS);
+            return true;
+        }
+        else return false;
+    }
+}
